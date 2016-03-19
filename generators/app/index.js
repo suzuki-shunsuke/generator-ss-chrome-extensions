ece1618be2fb4ef2d93603360f7b9ee914e334b9
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2016-03-19 15:50:04 +0900

    バグ修正

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,9 +1,9 @@
 'use strict';
 var yeoman = require('yeoman-generator');
 
-module.exports = yeoman.generators.Base.extend({
+module.exports = yeoman.Base.extend({
   writing: function () {
-    this.fs.directory(
+    this.directory(
       this.templatePath('.'),
       this.destinationPath('.')
     );
